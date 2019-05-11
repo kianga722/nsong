30503af32c2ed385f73f5fcedf89e9011286dd46
kianga722 <kianga722@gmail.com>
2019-05-11 18:31:15 -0400

    Added sorting by date

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SortBar from './components/SortBar';
 import SongList from './components/SongList';
 import ChannelBox from './components/ChannelBox';
 
@@ -8,13 +9,26 @@ class App extends Component {
 
     this.state = {
       songs: null,
+      sortDateNewest: true,
       channelSort: {
         Proximity: true,
         'Thrilling Music': true,
         'Revealed Music': true,
         WaveMusic: true,
-      }
+      },
     };
+  }
+
+  songsSet = (songsNew) => {
+    this.setState({
+      songs: songsNew
+    })
+  }
+
+  sortDateNewestToggle = (bool) => {
+    this.setState({
+      sortDateNewest: bool
+    })
   }
 
   toggleSort = (channel) => {
@@ -38,12 +52,35 @@ class App extends Component {
     })
   }
 
+  dateSortNewest = (bool) => {
+    let songsSorted = [...this.state.songs ];
+    songsSorted.sort((a, b) => {
+      const d1 = new Date(a.published);
+      const d2 = new Date(b.published);
+      if (bool) {
+        if (d1 >= d2) {
+          return -1;
+        }
+        if (d1 < d2) {
+          return 1;
+        }
+      } else {
+        if (d1 >= d2) {
+          return 1;
+        }
+        if (d1 < d2) {
+          return -1;
+        }
+      }
+    })
+    this.songsSet(songsSorted);
+    this.sortDateNewestToggle(bool); 
+  }
+
   async componentDidMount() {
     const songsGet = await fetch('/api/');
     const songsGetJSON = await songsGet.json();
-    this.setState({
-      songs: songsGetJSON
-    })
+    this.songsSet(songsGetJSON);
   }
 
   render() {
@@ -52,6 +89,10 @@ class App extends Component {
         {
           this.state.songs
           && <div id='content'>
+              <SortBar
+                sortDateNewest={this.state.sortDateNewest}
+                dateSortNewest={this.dateSortNewest}
+              />
               <SongList
                 channelSort={this.state.channelSort}
                 songs={this.state.songs}
---
+++ client/src/components/SortBar.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class SortBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdownDateShow: false
+    };
+  }
+
+  dropdownToggle = () => {
+    this.setState({
+      dropdownDateShow: !this.state.dropdownDateShow
+    })
+  }
+
+  renderDropdown = () => {
+    return (
+      <div className='dropdown'>
+        <a href='#'
+          className='dropdown-item'
+          onClick={() => {
+            this.props.dateSortNewest(true);
+            this.dropdownToggle();
+          }}
+        >
+          <span>Date (Newest First)</span>
+          {this.props.sortDateNewest ? <span>current</span> : null}
+        </a>
+        <a href='#'
+          className='dropdown-item'
+          onClick={() => {
+            this.props.dateSortNewest(false);
+            this.dropdownToggle();
+          }}
+        >
+          <span>Date (Oldest First)</span>
+          {this.props.sortDateNewest ? null : <span>current</span>}
+        </a>
+      </div>
+    )
+  }
+     
+  render() {
+    return (
+      <div id='sortBar'>
+        
+        <div id='dateSort'>
+          <a href='#'
+            className='dropdownToggle'
+            onClick={this.dropdownToggle}
+          >
+            {this.props.sortDateNewest ? 'Date (Newest First)' : 'Date (Oldest First)'}
+          </a>
+          {this.state.dropdownDateShow ? this.renderDropdown() : null}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SortBar;
